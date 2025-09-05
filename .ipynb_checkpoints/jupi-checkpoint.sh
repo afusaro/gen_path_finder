57f00c06faaea0eff4ccfceb96f441545c8d7d3b
agustin-fusaro-datadog <agustin.fusaro@datadoghq.com>
2025-09-05 09:21:36 +0200

    Fix paths

--- .ipynb_checkpoints/jupi-checkpoint.sh
+++ .ipynb_checkpoints/jupi-checkpoint.sh
@@ -1,5 +1,5 @@
 docker run --name jupyter -it -p 8888:8888 \
-  -u 1000 -v /Users/agustinfusaro/uavia/gen_path_finder:/tmp/notebooks \
+  -u 1000 -v /Users/agustin.fusaro/perso/gen_path_finder:/tmp/notebooks \
   -e HOME=/tmp/jupyter python:3.8 bash -c "
     mkdir /tmp/jupyter; \
     pip install --user 'jupyterlab < 4' 'ipympl < 0.8' pandas matplotlib gaft; \
